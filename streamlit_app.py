0135f65bd5580dd489de0921f166eb1ee0067b9a
PradHolla <pradholla@gmail.com>
2022-01-27 22:09:48 +0530

    Code optimization

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,7 +9,6 @@ st.subheader("User Input Features")
 @st.cache
 def load_data(df1, df2):
     data = pd.read_csv(df1)
-    # data.columns = data.columns.str.replace(' ', '')
     cos_sim_data = pd.read_pickle(df2)
     series_title = data['Series_Title'].to_list()
     series_title.insert(0,'')
@@ -22,26 +21,29 @@ def give_recommendations(index,print_recommendation=False,print_recommendation_p
     if print_recommendation==True:
         st.write('The watched movie is this one: %s \n'%(data['Series_Title'].loc[index]))
         for k, movie in enumerate(movies_recomm, start=1):
-            st.write('The number %i recommended movie is this one: %s \n'%(k,movie))
+            st.header('The number %i recommended movie is this one: %s \n'%(k,movie))
+            # for k, q in enumerate(range(len(movies_recomm)), start=1):
+            #     plot_q = data['Overview'].loc[index_recomm[q]]
+            #     genre = data['Genre'].loc[index_recomm[q]]   
+            #     st.subheader('Plot:  \n %s \n'%(plot_q))
+            #     st.subheader('Genre:  \n %s \n'%(genre))
+            #     q
     if print_recommendation_plots==True:
-        st.write('The plot of the watched movie is this one:\n %s \n'%(data['Overview'].loc[index]))
+        # st.write('The plot of the watched movie is this one:  \n %s \n'%(data['Overview'].loc[index]))
         for k, q in enumerate(range(len(movies_recomm)), start=1):
             plot_q = data['Overview'].loc[index_recomm[q]]
-            st.write('The plot of the number %i recommended movie is this one:\n %s \n'%(k,plot_q))
+            st.write('The plot of the number %i recommended movie is this one:  \n %s \n'%(k,plot_q))
     if print_genres==True:
-        st.write('The genres of the watched movie is this one:\n %s \n'%(data['Genre'].loc[index]))
+        # st.write('The genres of the watched movie is this one:\n %s \n'%(data['Genre'].loc[index]))
         for k, q in enumerate(range(len(movies_recomm)), start=1):
             plot_q = data['Genre'].loc[index_recomm[q]]
-            st.write('The plot of the number %i recommended movie is this one:\n %s \n'%(k,plot_q))
+            st.write('The plot of the number %i recommended movie is this one:  \n %s \n'%(k,plot_q))
     return result
 
 data, cos_sim_data, series_title = load_data('data.csv', 'cos_sim_data.pkl')
 
 movie_name = st.selectbox("Enter a movie name: ", (series_title))
 btn = st.button("Recommendation")
-#list comprehension
-# movie_name = [s for s in series_title[:10]]
-
 st.write(movie_name)
 if btn:
     for idx, elm in enumerate(series_title[1:]):
@@ -49,16 +51,3 @@ if btn:
             st.write(idx)
             ret = (give_recommendations(idx,True,True,True))
             
-
-
-
-
-# # result = give_recommendations(idx,True)
-# # st.write(result)
-
-# idrec = cos_sim_data.loc[idx].sort_values(ascending=False).index.tolist()[1:6]
-# data['Series_Title'].loc[idrec].values
-# # data['Series_Title'][998]
-
-# data = [[data]]
-# data
