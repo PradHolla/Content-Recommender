f0eb66a45677f48f5acce453f2abe8634caa91aa
PradHolla <pradholla@gmail.com>
2022-02-01 22:15:46 +0530

    Added link to GitHub

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import pandas as pd
 from PIL import Image
-st.set_page_config(page_title="Movie Recommender System", page_icon="🎬", layout="wide")
 
+st.set_page_config(page_title="Movie Recommender System", page_icon="🎬", layout="wide")
 background = Image.open("Data/clapper.png")
 col1, col2, col3 = st.columns([3, 3, 1])
 col2.image(background, width=150)
-# st.image("Data/clapper.png", width=200)
+
 st.header("Movie Recommender")
+st.write("Created by [PNH](https://github.com/PradHolla). Powered by [Streamlit](https://streamlit.io/).")
 st.write("""
 ##### This app recommends 5 similar movies to the one you watched. Just enter the movie title and click on the button.
 ##### This app is based on the IMDb 1000 movies dataset. The dataset is available [here](https://www.kaggle.com/harshitshankhdhar/imdb-dataset-of-top-1000-movies-and-tv-shows).\
